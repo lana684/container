606650a021c48e145cf88e4a64269070f2692c33
lana684 <162273594+lana684@users.noreply.github.com>
2024-04-05 21:20:50 +0500

    Add files via upload

--- GroupContainer.h
+++ GroupContainer.h
@@ -7,12 +7,16 @@ class GroupContainer: public Container
 public:
     GroupContainer(MemoryManager &mem): Container(mem) {}
 
-    virtual size_t hash_function(void* key, size_t keySize)
-    {
-        return 0;
-    }
+	virtual size_t hash_function(void* key, size_t keySize)
+	{
+		int hash = 0;
+		char* k = (char*)key;
+		for (int i = 0; i < keySize; i++)
+		{
+			hash = (hash << 5) - hash + k[i];
+		}
+		return hash;
+	}
 };
 
 //разрешается менять
-
-// message
