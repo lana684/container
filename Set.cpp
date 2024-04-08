03c505eaa2ec3858a16a24aa3d9d2bd39a2c703e
dana-di <161107505+dana-di@users.noreply.github.com>
2024-04-08 21:42:37 +0500

    Add files via upload

--- Set.cpp
+++ Set.cpp
@@ -4,75 +4,198 @@
 #include <stdlib.h>
 #include <iostream>
 #include <list>
-#include "MemoryManager.h"
 #include "Mem.h"
 #include "Set.h"
 
-    void* Set::Iterator::getElement (size_t &size)
-    {
 
-        return NULL;
-    }
-    // Возвращает true, если есть следующий элемент, иначе false.
-    bool Set::Iterator::hasNext()
+Set::Iterator::Iterator(Set* set)
+{
+    this->index = 0;
+    this->set = set;
+    this->curent = set->list_arr[index];
+    this->iter_l = this->set->list_arr[this->index]->newIterator();
+    this->end_i = this->set->hash_size; this->end_i--;
+    this->end = set->list_arr[this->end_i]->newIterator(); // оптимизация?
+}
+
+Set::Iterator::~Iterator()
+{
+    this->set = NULL;
+    this->curent = NULL;
+    delete this->iter_l;
+    delete this->end;
+}
+
+void* Set::Iterator::getElement(size_t& size)
+{
+    return this->iter_l->getElement(size);
+}
+
+bool Set::Iterator::hasNext()
+{
+    int tmp_i = this->index;
+    void* tmp_list = this->curent;
+    if (this->iter_l->hasNext() == true)
     {
         return true;
     }
-    // Переход к следующему элементу.
-    void Set::Iterator::goToNext()
-    {}
-    // проверка на равенство итераторов
-    bool Set::Iterator::equals(Container::Iterator* right)
+    else if (this->iter_l->hasNext() == false && this->iter_l != this->end )
     {
-        return true;
-    }
-
-    int Set::size(){
-        return this->set_size;
-    }
-    size_t Set::max_bytes(){
-        return this->bytes_size;
-    }
-    Container::Iterator* Set::find(void *elem, size_t size)
-    {
-        return 0;
-    }
-    Container::Iterator* Set::newIterator()
-    {
-        return 0;
-    }
-    void Set::remove(Container::Iterator *iter)
-    {
-
-    }
-    //приведение типов
-    void Set::clear()
-    {
-    }
-    bool Set::empty()
-    {
-        if (this->bytes_size == 0 && this->set_size == 0)
+        while (tmp_list == NULL || tmp_i != this->set->hash_size - 1)
+        {
+            tmp_i++;
+            tmp_list = this->set->list_arr[tmp_i];
+        }
+        if (tmp_list)
         {
             return true;
         }
-        else {
-            return false;
+        else { return false; }
+    }
+}
+
+void Set::Iterator::goToNext()
+{
+    if (this->hasNext() == true)
+    {
+        if (this->iter_l->hasNext() == false && this->iter_l != this->end)
+        {
+            while (this->iter_l == NULL || this->index != this->set->hash_size - 1)
+            {
+                this->index++; 
+                this->iter_l->goToNext();
+                this->curent = this->set->list_arr[this->index];
+            }
+        }
+        else if (this->iter_l->hasNext() == true)
+        {
+            this->iter_l->goToNext();
         }
     }
+}
 
-    int Set::insert(void *elem, size_t size)
+// +-
+bool Set::Iterator::equals(Container::Iterator* right)
+{
+    Set::Iterator* iter_r = dynamic_cast<Set::Iterator*>(right);
+    if (this->iter_l->equals(iter_r->iter_l) == true)
     {
-        return 0;
+        return true;
     }
+    else { return false; }
+}
 
-    size_t Set::hash_function(void* key, size_t keySize)
+void Set::Iterator::find_end(size_t& indx_end)
+{
+    size_t max_indx = this->set->hash_size;
+    max_indx--;
+    for (size_t j = max_indx; j >= 0; j--)
     {
-        size_t h = 0;
-        return h;
+        if (this->set->list_arr[j] != NULL)
+        {
+            indx_end = j;
+            size_t size = 0;
+            void* elem = this->set->list_arr[j]->front(size);
+            this->end = this->set->list_arr[j]->find(elem, size);
+            return;
+        }
     }
+    return;
+}
 
-    int Set::get_hash_size() {
-        return 0;
+/*void Set::Iterator::change_index(int new_indx)
+{
+    this->index = new_indx;
+}
+int Set::Iterator::get_index()
+{
+    return this->index;
+}*/
+
+int Set::size() {
+    return this->set_size;
+}
+size_t Set::max_bytes() {
+    return _memory.size();
+}
+
+//+-
+Container::Iterator* Set::find(void* elem, size_t size)
+{
+    size_t hash = this->hash_function(elem, size);
+    // dynamic_cast<тип_в_который_преобразуем>(преобразуемый_объект)
+    Set::Iterator* Iter = dynamic_cast<Set::Iterator*>(this->newIterator());//???
+    if (Iter->index != hash) {
+        Iter->index = hash;
+        Iter->curent = this->list_arr[hash];
     }
+    Iter->iter_l = Iter->set->list_arr[hash]->find(elem, size);
+    Iter->find_end(Iter->end_i);
+    if (Iter->iter_l == nullptr)
+    {
+        delete Iter;
+        return nullptr;
+    }
+    else { return Iter; }
+}
 
+Container::Iterator* Set::newIterator()
+{
+    if (this->set_size == 0) { return nullptr; }
+    else
+    {
+        Set::Iterator* Iter = new Set::Iterator(this);
+        return Iter;
+    }
+}
+
+//--
+void Set::remove(Container::Iterator* iter)
+{
+
+}
+    //приведение типов
+
+//--
+void Set::clear()
+{
+}
+
+bool Set::empty()
+{
+    if (this->set_size == 0)
+    {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+//--
+int Set::insert(void* elem, size_t size)
+{
+    size_t hash = Set::hash_function(elem, size);
+    Set::Iterator* iter = new Set::Iterator(this);
+    if (Set::empty() == true)
+    {
+        
+        
+
+
+    }
+    else
+    {
+    }
+    return 0;
+}
+
+size_t Set::hash_function(void* value, size_t valueSize)
+{
+    size_t h = 0;
+
+    return h;
+}
+
+    // хэш номер списка, остаток от деления на количество списков
 
--- Set.h
+++ Set.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <list>
+#include "List.h"
 #include "Mem.h"
 #include "SetAbstract.h"
 
@@ -11,14 +12,29 @@ using namespace std;
 
 class Set :public AbstractSet {
     int set_size;
-    size_t bytes_size;
     int hash_size;
-    list<int> list_arr[];
+    List** list_arr; //List* -тип, * массив списков
     public:
-    Set(MemoryManager &mem) :AbstractSet(mem) {}
+    Set(MemoryManager &mem, int hash_size) :AbstractSet(mem) {
+        this->set_size = 0;
+        this->hash_size = hash_size;
+        List** arr = (List**)_memory.allocMem(sizeof(List*)*hash_size);//разбор
+    }
 
-    class Iterator: public Container::Iterator
+    class Iterator : public Container::Iterator
     {
+        Set* set;
+        size_t index;
+        void *curent;
+        size_t end_i;
+        Container::Iterator* end;
+        //List::Iterator *end;
+        Container::Iterator* iter_l;
+        //List::Iterator* iter_l; // указатель на итератор списка 0
+    public:
+        Iterator(Set* set);
+        // Возврашает явно указатель на элемент, на который указывает итератор в данный момент.
+        // Неявно возвращает размер данных.
         void* getElement(size_t& size);
         // Возвращает true, если есть следующий элемент, иначе false.
         bool hasNext();
@@ -26,6 +42,12 @@ class Set :public AbstractSet {
         void goToNext();
         // проверка на равенство итераторов
         bool equals(Container::Iterator* right);
+        // Возвращает неявно индекс последнего заполненого листа. 
+        // Перемещает указатель конца на последний элемент этого списка.
+        // Явно возращает 1 в случае успеха и 0 неудачи.
+        void find_end(size_t &index_end);
+        friend class Set;
+        ~Iterator();
     };
 
     int size() override;
@@ -36,10 +58,13 @@ class Set :public AbstractSet {
     void clear() override;
     bool empty() override;
 
+
+    // Добавление элемента в контейнер в соответствующую позицию, в соответствии с выбором реализации множества.
+    // В случае успешного добавления функция возвращает значение 0, если такой элемент уже есть в контейнере - 1, во всех остальных случаях - 2.
     int insert(void *elem, size_t size) override;
 
     // вынести в общий
-    size_t hash_function(void* key, size_t keySize);
+    size_t hash_function(void* key_or_value, size_t key_or_value_Size);
 
-    int get_hash_size();
+    friend class Set::Iterator;
 };
